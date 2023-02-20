5e344812423eb009107cd6b2c10aafe38f7e20ba
Duane <duane.edmonds@gmail.com>
2023-02-19 19:03:37 -0800

    (automated gcp push)

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useState } from "react";
 import { Box, Card, Tabs, Tab, Typography } from "@mui/material";
 import PropTypes from "prop-types";
-import randd from "../assets/randd.jpg";
 import AboutMe from "./AboutMe";
 import Contact from "./Contact";
 import MedDev from "./MedDev";
@@ -58,61 +57,43 @@ function Content() {
       >
         <Tab label="About Me" {...a11yProps(0)} sx={{ color: "#555555" }} />
         <Tab
-          label="Research & Development"
+          label="Medical Device Development"
           {...a11yProps(1)}
           sx={{ color: "#555555" }}
         />
         <Tab
-          label="Medical Device Development"
+          label="Laboratory Automation"
           {...a11yProps(2)}
           sx={{ color: "#555555" }}
         />
-        <Tab
-          label="Laboratory Automation"
-          {...a11yProps(3)}
-          sx={{ color: "#555555" }}
-        />
-        <Tab label="Compliance" {...a11yProps(4)} sx={{ color: "#555555" }} />
-        <Tab
-          label="Coaching & Leadership"
-          {...a11yProps(5)}
-          sx={{ color: "#555555" }}
-        />
+        <Tab label="Compliance" {...a11yProps(3)} sx={{ color: "#555555" }} />
         <Tab
           label="Work Experience"
-          {...a11yProps(6)}
+          {...a11yProps(4)}
           sx={{ color: "#555555" }}
         />
-        <Tab label="Get In Touch" {...a11yProps(7)} sx={{ color: "#555555" }} />
+        <Tab label="Get In Touch" {...a11yProps(5)} sx={{ color: "#555555" }} />
       </Tabs>
       <TabPanel value={value} index={0}>
         <AboutMe />
       </TabPanel>
       <TabPanel value={value} index={1} className="selection-heading">
-        <img src={randd} height={"650px"} />
-      </TabPanel>
-      <TabPanel value={value} index={2} className="selection-heading">
         <MedDev />
       </TabPanel>
       <TabPanel
         value={value}
-        index={3}
+        index={2}
         className="selection-heading"
       ></TabPanel>
-      <TabPanel value={value} index={4} className="selection-heading">
+      <TabPanel value={value} index={3} className="selection-heading">
         <Compliance />
       </TabPanel>
       <TabPanel
         value={value}
-        index={5}
+        index={4}
         className="selection-heading"
       ></TabPanel>
-      <TabPanel
-        value={value}
-        index={6}
-        className="selection-heading"
-      ></TabPanel>
-      <TabPanel value={value} index={7} className="selection-heading">
+      <TabPanel value={value} index={5} className="selection-heading">
         <Contact />
       </TabPanel>
       <br />
